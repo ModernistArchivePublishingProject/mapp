7cdbfe2bfd8519f83842694a163947b37029fd7a
Mike Widner <mikewidner@gmail.com>
2015-04-15 03:37:18 +0000

    DB fix script added

---
+++ scripts/update_db.sql
@@ -0,0 +1,8 @@
+drop table field_data_field_birth_date;
+drop table field_data_field_death_date;
+drop table field_data_field_relationships;
+drop table field_data_field_relationship_type;
+drop table field_revision_field_birth_date;
+drop table field_revision_field_death_date;
+drop table field_revision_field_relationships;
+drop table field_revision_field_relationship_type;
