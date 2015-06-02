70092db0e940c317c65d46b76133a787ece24440
Mike Widner <mikewidner@gmail.com>
2015-06-02 03:57:43 +0000

    Code upgrades

--- scripts/update_db.sql
+++
@@ -1,8 +0,0 @@
-drop table field_data_field_birth_date;
-drop table field_data_field_death_date;
-drop table field_data_field_relationships;
-drop table field_data_field_relationship_type;
-drop table field_revision_field_birth_date;
-drop table field_revision_field_death_date;
-drop table field_revision_field_relationships;
-drop table field_revision_field_relationship_type;
